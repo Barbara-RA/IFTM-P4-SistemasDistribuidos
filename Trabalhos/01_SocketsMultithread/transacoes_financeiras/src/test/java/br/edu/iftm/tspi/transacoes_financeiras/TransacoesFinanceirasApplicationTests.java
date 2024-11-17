33f108f1ec6dd85584de32e3dcbe19ed21dd825f
Barbara-RA <barbara.ramosalves94@gmail.com>
2024-11-17 12:17:27 -0300

    backup ultimas aulas

---
+++ Trabalhos/01_SocketsMultithread/transacoes_financeiras/src/test/java/br/edu/iftm/tspi/transacoes_financeiras/TransacoesFinanceirasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.tspi.transacoes_financeiras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacoesFinanceirasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
